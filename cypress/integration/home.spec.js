0f935d4a722ed15eb39e5a993fddaa55c65db85d
Faiem-qy <f.quanyoung@gmail.com>
2024-01-09 20:45:39 -0500

    add category and products

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,6 @@
 
 it('should display a list of products', () => {
-  cy.visit('http://localhost:3000/'); // Visit the home page
-
-  // Check if the product list exists on the home page
+  cy.visit('http://localhost:3000/');
   cy.get('.products article').should("have.length.gt", 10);
 });
+
